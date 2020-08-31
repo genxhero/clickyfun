4198fc48606f5a1d4754542326ac8da9a15afab6
genxhero <genxhero@users.noreply.github.com>
2020-08-31 12:41:14 -0700

    fix useeffect

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import './App.css';
 import Dot from './Dot';
 
@@ -6,13 +6,27 @@ const App = props => {
 
   const [dots, updateDots] = useState([])
 
+  useEffect(() => {
+    const interval = setInterval(() => {
+      console.log("Dots:", dots)
+      const movedDots = [];
+      for (let i = 0; i < dots.length; i++) {
+        movedDots.push({ x: dots[i].x, y: dots[i].y - 10 })
+      }
+      console.log("New Dots", movedDots)
+      updateDots(movedDots);
+    }, 200);
+    return () => clearInterval(interval);
+  }, [dots]);
+
   const handleClick = event => {
+    // debugger;
     const x = event.clientX;
     const y = event.clientY;
     const newDots = dots.concat([{x: x, y: y}])
     updateDots(newDots);
   }
-
+  
   return (
     <div className="App" onClick={handleClick}>
         {
--- src/Dot.jsx
+++ src/Dot.jsx
@@ -1,14 +1,17 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 const Dot = props => {
-
+   
     const {x, y} = props;
+    //change back to fixed if we can manage all state in app component
+    const [style, setStyle] = useState({
+        "position": "absolute",
+        "left": `${x}px`,
+        "top": `${y}px`
+    })
+
     return (
-        <div className="dot" 
-           style={{ "position": "absolute", "left": `${x}px`, "top": `${y}px` }}
-           >
-            O
-        </div>
+        <div className="dot" style={style} />
     )
 }
 
